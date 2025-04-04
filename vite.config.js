2b57c5473739c7c6caacdb3cf8a4e20d38875b71
HoangVV <hoang.vu@logigear.com>
2025-04-04 14:42:02 +0700

    update build

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://github.com/viethoang0707/devops",
+  base: '/devops/',
   plugins: [
     vue(),
     vueDevTools(),
